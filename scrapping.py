75db2ce13817ead7f4b341349427789c1587acdc
sagarsangwan <iamsagarsangwan@gmail.com>
2021-12-18 15:14:52 +0530

    finding all link in webpage

--- scrapping.py
+++ scrapping.py
@@ -4,6 +4,13 @@ from bs4 import BeautifulSoup
 req = requests.get("https://sagarsangwan.herokuapp.com/")
 soup = BeautifulSoup(req.content, "html.parser")
 
-print(soup.prettify())
-print(soup.get_text())
+# print(soup.prettify())
+# print(soup.get_text())
+# print(soup.title.string)
+print(soup.find_all("a"))
+# print(soup.p)
+# print(soup.find("link"))
 
+link = soup.find_all("a")
+for i in link:
+    print(i.get("href"))
