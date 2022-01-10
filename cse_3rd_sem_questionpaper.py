541e7ff323481b6d9478abbbbd021df8922d7217
sagarsangwan <iamsagarsangwan@gmail.com>
2022-01-10 17:44:36 +0530

    not working properly

--- cse_3rd_sem_questionpaper.py
+++ cse_3rd_sem_questionpaper.py
@@ -48,30 +48,37 @@ print("before===============")
  
 
 def upload_to_mysql(info):
+    subject_ids = []
     for n in info:
         cur = mydb.cursor()
-        cur.execute("INSERT INTO question_paper(qp_name, link, year) values(%s, %s, %s)", (str((n.get("name"))), str(n.get("link")), str(n.get("year"))))
-        cur.execute("SELECT LAST_INSERT_ID();")
-        id =cur.fetchone()[0]
-        cur.execute("INSERT INTO subjects(subject_name, subject_desc, question_papers) values(%s, %s, %s)", (str((n.get("name"))),str((n.get("desc"))), str(id)))
-        
-    mydb.commit()
-    cur.close()
-    return"done"
+        cur.execute("SELECT qp_name FROM question_paper")
+        data = list(map(lambda x: x[0], cur.fetchall()))
 
-def upload(info):
-    for n in info:
-        cur = mydb.cursor()
-        cur.execute("SELECT * FROM temp")
-        data = cur.fetchall()
-        if n in data:
-            pass
+        if n.get("name") in data:
+            print("skipped-", n)
         else:
-            cur.execute("INSERT INTO temp(name, sub_desc) values(%s, %s)", (str((n.get("name"))), str(n.get("year"))))
+            cur.execute("INSERT INTO question_paper(qp_name, link, year) values(%s, %s, %s)", (str((n.get("name"))), str(n.get("link")), str(n.get("year"))))
+            cur.execute("SELECT LAST_INSERT_ID();")
+            id =cur.fetchone()[0]
+            cur.execute("INSERT INTO subjects(subject_name, subject_desc, question_papers) values(%s, %s, %s)", (str((n.get("name"))),str((n.get("desc"))), str(id)))
+            cur.execute("SELECT LAST_INSERT_ID();")
+            sub_id =cur.fetchone()[0]
+            subject_ids.append(str(sub_id))
+            cur.execute("INSERT INTO subjects_to_course(subject_id, course_ids) values(%s, %s)",(str(sub_id), "1"))
+            print("inserted-", n)
+
+    print("-------------", ",".join(subject_ids))
+    cur.execute("UPDATE course_to_subjets SET subjects_ids = subjects_ids+ %s WHERE course_id = %s", (",".join(subject_ids), "1"))
     cur.close()
     return"done"
 
-# upload()
+# cur = mydb.cursor()
+# cur.execute("SELECT * FROM temp")
+# data = cur.fetchall()
+# print(data)
+# cur.close()
+
+
 
 upload_to_mysql(final_links(get_all_subject_link()))
 print("after=================================")
