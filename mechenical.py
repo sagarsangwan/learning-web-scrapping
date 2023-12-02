6cf52b45c955b813cec1da1522339e94bce186d8
sagar sangwan <iamsagarsangwan@gmail.com>
2023-12-02 22:33:37 +0530

    project updated added requirements.txt and env variables

--- mechenical.py
+++ mechenical.py
@@ -3,11 +3,15 @@ from bs4 import BeautifulSoup
 import mysql.connector
 
 
+import os
+from dotenv import load_dotenv
+
+load_dotenv(".env")
 mydb = mysql.connector.connect(
-    host='localhost',
-    user='sagar',
-    password="password",
-    database="student-portal"
+    host=os.environ.get("MYSQL_HOST"),
+    user=os.environ.get("MYSQL_USER"),
+    password=os.environ.get("MYSQL_PASSWORD"),
+    database=os.environ.get("MYSQL_DB"),
 )
 
 
@@ -21,13 +25,14 @@ def get_all_subject_link():
 
     return sem_link
 
+
 def final_links(links):
     data = []
     for link in links:
         req = requests.get(link)
         soup = BeautifulSoup(req.content, "html.parser")
         item_card = soup.findAll("a", {"class": "br-thumb"})
-        
+
         for i in item_card:
             name = (i.select(".br-thumb h6.text-dark span"))[0].string
             desc = i["title"]
@@ -40,12 +45,12 @@ def final_links(links):
                 "link": link,
             }
             data.append((dic))
-        
-    return data 
+
+    return data
+
+
 print("before===============")
 
-        
- 
 
 def upload_to_mysql(info):
     subject_ids = []
@@ -57,24 +62,35 @@ def upload_to_mysql(info):
         if n.get("name") in data:
             print("skipped-", n.get("name"))
         else:
-            cur.execute("INSERT INTO question_paper(qp_name, link, year) values(%s, %s, %s)", (str((n.get("name"))), str(n.get("link")), str(n.get("year"))))
+            cur.execute(
+                "INSERT INTO question_paper(qp_name, link, year) values(%s, %s, %s)",
+                (str((n.get("name"))), str(n.get("link")), str(n.get("year"))),
+            )
             cur.execute("SELECT LAST_INSERT_ID();")
-            id =cur.fetchone()[0]
-            cur.execute("INSERT INTO subjects(subject_name, subject_desc, question_papers) values(%s, %s, %s)", (str((n.get("name"))),str((n.get("desc"))), str(id)))
+            id = cur.fetchone()[0]
+            cur.execute(
+                "INSERT INTO subjects(subject_name, subject_desc, question_papers) values(%s, %s, %s)",
+                (str((n.get("name"))), str((n.get("desc"))), str(id)),
+            )
             cur.execute("SELECT LAST_INSERT_ID();")
-            sub_id =cur.fetchone()[0]
+            sub_id = cur.fetchone()[0]
             subject_ids.append(str(sub_id))
-            cur.execute("INSERT INTO subjects_to_course(subject_id, course_ids) values(%s, %s)",(str(sub_id), "2"))
+            cur.execute(
+                "INSERT INTO subjects_to_course(subject_id, course_ids) values(%s, %s)",
+                (str(sub_id), "2"),
+            )
             print("inserted-", n.get("name"))
 
-    if len(subject_ids) > 0: 
-        cur.execute("INSERT INTO course_to_subjets(subjects_ids, course_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE subjects_ids=subjects_ids+%s", (",".join(subject_ids), "2", ",".join(subject_ids)))
+    if len(subject_ids) > 0:
+        cur.execute(
+            "INSERT INTO course_to_subjets(subjects_ids, course_id) VALUES (%s, %s) ON DUPLICATE KEY UPDATE subjects_ids=subjects_ids+%s",
+            (",".join(subject_ids), "2", ",".join(subject_ids)),
+        )
 
     # cur.execute("UPDATE course_to_subjets SET subjects_ids = subjects_ids+ %s WHERE course_id = %s", (",".join(subject_ids), "1"))
     mydb.commit()
     cur.close()
-    return"done"
-
+    return "done"
 
 
 upload_to_mysql(final_links(get_all_subject_link()))
